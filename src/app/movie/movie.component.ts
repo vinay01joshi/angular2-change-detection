24646a8a608ca25fc2c6095260f14989914f157b
Vinay Joshi <vinay01joshi@gmail.com>
2017-07-20 11:47:44 +0530

    :bookmark: inital commit

---
+++ src/app/movie/movie.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'movie',
+  templateUrl: './movie.component.html',
+  styleUrls: ['./movie.component.css']
+})
+export class MovieComponent {
+  @Input() movie; 
+}
---
+++ src/app/movies/movies.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'movies',
+  templateUrl: './movies.component.html',
+  styleUrls: ['./movies.component.css']
+})
+export class MoviesComponent  {
+  @Input() movies; 
+}
