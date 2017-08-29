1fff7ae8a13fd50f7a9766d25b83749ad2b1deea
Vinay Joshi <vinay01joshi@gmail.com>
2017-08-29 19:06:36 +0530

    Change Detechtion | Immutable.js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, DoCheck } from '@angular/core';
+import { Map, List } from 'immutable';
 
 @Component({
   selector: 'app-root',
@@ -8,17 +9,25 @@ import { Component, DoCheck } from '@angular/core';
 export class AppComponent implements DoCheck {
   title = 'Change Detection';
   movies = [
-    {title: 'Movie 1'},
-    {title: 'Movie 2'},
-    {title: 'Movie 3'},
-    {title: 'Movie 4'},
+    Map({ title: 'Movie 1', genre: 1}),
+    Map({ title: 'Movie 2', genre: 1}),
+    Map({ title: 'Movie 3', genre: 1}),
+    Map({ title: 'Movie 4', genre: 1})
   ];
+  constructor(){
+    var movie  = Map({ title: 'M1', genre: 1});
+    var title = movie.get('title');
+    movie = movie.set('title','NEW TITLE');
+    movie.toJS();
+    console.log(movie.toJS());
+  }
 
   ngDoCheck() {
     console.log("AppComponent-DoCheck");
   }
 
   onClick(){
-    this.movies[0] = {title: 'UPDATED'};
+    var movie = this.movies[0];
+    this.movies[0]= movie.set('title','updated');
   }
 }
